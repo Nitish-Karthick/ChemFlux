69de99b38e533ece1e0faf79ccdb64c2ac743148
Nitish <nitishkarthick279@gmail.com>
2025-11-17 15:11:03 +0530

    Relax CORS for Netlify demo

--- backend/chemflux_backend/settings.py
+++ backend/chemflux_backend/settings.py
@@ -91,13 +91,7 @@ REST_FRAMEWORK = {
     ],
 }
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',
-    'http://127.0.0.1:5173',
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-    'https://chemflux.netlify.app',
-]
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:5173',
